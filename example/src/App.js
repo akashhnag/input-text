3119a95691e1a4915b56ea77f844dc6d38cad564
Cognitifai <akash.nag@cognitif.ai>
2020-03-04 15:27:43 +0530

    first commit

--- example/src/App.js
+++ example/src/App.js
@@ -1,13 +1,39 @@
 import React, { Component } from 'react'
 
-import ExampleComponent from 'custom-input'
+import InputBox from 'custom-input'
 
 export default class App extends Component {
   render () {
+    let style={
+      marginTop:'15px',
+      marginLeft:'20px'
+    }
     return (
       <div>
-        <ExampleComponent text='Modern React component module' />
+        <React.Fragment>
+        <div style={style}>Password Textbox:<InputBox type='password' getInput={this.getInput} min-length={5}/></div>
+        </React.Fragment>
+        <React.Fragment>
+          <div style={style}>
+          Simple Textbox:<InputBox type='text' getInput={this.getInput}/>
+          </div>
+        </React.Fragment>
+        <React.Fragment>
+          <div style={style}>
+          Email Textbox:<InputBox type='email' getInput={this.getInput} errMsg='incorrect email format'/>
+          </div>
+        </React.Fragment>
       </div>
     )
   }
+
+  getInput(data){
+    console.log('data',data);
+
+  }
+
 }
+
+// InputBox.errorMessage({
+//   msg:'invalid password'
+// });
--- src/index.js
+++ src/index.js
@@ -1,22 +1,86 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 
-import styles from './styles.css'
-
-export default class ExampleComponent extends Component {
+var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+export default class InputBox extends Component {
+  constructor(props){
+    super(props);
+    this.state={
+      input:'',
+      shortPassword:false,
+      errorMessage:'',
+      invalidEmail:false
+    }
+  }
   static propTypes = {
-    text: PropTypes.string
+    'type': PropTypes.string,
+    'min-length':PropTypes.number,
+    'errMsg':PropTypes.string
   }
 
   render() {
     const {
-      text
+      type
     } = this.props
 
     return (
-      <div className={styles.test}>
-        Example Component: {text}
-      </div>
+      <React.Fragment>
+        <input type={type} onChange={this.handleInput} onBlur={this.handleBlur} ></input>
+        {this.state.shortPassword ? <p>{this.props.errMsg || 'Invalid Input'}</p>:null}
+        {this.state.invalidEmail ? <p>{this.props.errMsg || 'Invalid Input'}</p>:null}
+      </React.Fragment>
+
     )
   }
+
+  handleInput=(e)=>{
+    this.setState({
+      input:e.target.value
+    } ,()=>{
+      this.props.getInput(this.state.input);
+
+    })
+  }
+
+  handleBlur=()=>{
+    //console.log(this.state.input.length);
+    console.log('props',this.props);
+
+    if(this.props.hasOwnProperty('min-length') && this.state.input.length<this.props['min-length']){
+      this.setState({
+        shortPassword:true
+      })
+    }
+
+    if(this.props.type==='email'){
+
+      if(!this.state.input.match(mailformat)){
+
+          this.setState({
+            invalidEmail:true
+          })
+
+
+      //  this.setState({
+      //    invalidEmail:false
+      //  })
+      // }else
+
+    }
+  }
+}
+
+  handleFocus=()=>{
+    console.log('focus',this.props);
+
+  }
+
+  // static errorMessage=(ob)=>{
+  //   console.log('error message',ob);
+  //   console.log(ob['msg']);
+  //   this.setState({
+  //     errorMessage:ob['msg']
+  //   })
+
+  // }
 }
